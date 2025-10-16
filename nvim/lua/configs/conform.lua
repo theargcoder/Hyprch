f74f4fbb256bfcb761f24ba75a7ee9f334d1ab37
Lucca Labattaglia <lucca.labattaglia@gmail.com>
2025-10-16 17:12:07 -0600

    added nvim and kitty config

---
+++ nvim/lua/configs/conform.lua
@@ -0,0 +1,26 @@
+local options = {
+  formatters_by_ft = {
+    lua = { "stylua" },
+    css = { "prettier" },
+    html = { "prettier" },
+    json = { "biome" },
+    javascript = { "biome" },
+    typescript = { "biome" },
+    jsonc = { "biome" },
+    bash = { "beautysh" },
+    csh = { "beautysh" },
+    ksh = { "beautysh" },
+    sh = { "beautysh" },
+    zsh = { "beautysh" },
+    cpp = { "clang-format" },
+    c = { "clang-format" },
+  },
+
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+}
+
+return options
