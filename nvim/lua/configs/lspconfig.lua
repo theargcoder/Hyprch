4a507f9795cf2171e00e008d12aa9346ee3728b2
Lucca Labattaglia <lucca.labattaglia@gmail.com>
2025-11-17 23:23:33 -0600

    new nvim config, much better and clangd works nicely nowgit add . (needs sudo pacman -S clang) as dependency

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,12 +1,25 @@
+-- lua/configs/lspconfig.lua
+
 -- Load NvChad defaults
 require("nvchad.configs.lspconfig").defaults()
 
--- Define servers
-local servers = { "clangd", "cmake" }
+-- NEW API (Neovim 0.11+)
+-- No require("lspconfig").
 
--- Enable servers with custom opts
-vim.lsp.enable(servers, {
-  clangd = {
-    cmd = { "clangd", "--offset-encoding=utf-16" },
+vim.lsp.config.clangd = {
+  cmd = {
+    "clangd",
+    "--clang-tidy",
+    "--clang-tidy-checks=clang-analyzer-*,bugprone-*,modernize-*,performance-*,readability-*,cppcoreguidelines-*,misc-*,cert-*,hicpp-*",
+    "--background-index",
+    "--all-scopes-completion",
+    "--header-insertion=never",
+    "--compile-commands-dir=build",
+    "--offset-encoding=utf-8",
   },
-})
+  filetypes = { "c", "cpp", "objc", "objcpp", "h", "hpp" },
+  root_dir = vim.fs.root(0, { "compile_commands.json", ".clangd", ".clang-tidy", ".git" }),
+}
+
+-- enable servers
+vim.lsp.enable { "clangd" }
